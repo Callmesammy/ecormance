d6519cb479e88d27a56b1a495dd23ee58a4a3546
Callmesammy <callmesammy@yahoo.com>
2023-09-13 22:54:25 +0100

    zz

---
+++ src/data/product.js
@@ -0,0 +1,43 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Nike Slim Shirt',
+        image: '/images/p1.jpg',
+        description: 'High quality product',
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '1',
+        name: 'Nike Slim Shirt',
+        image: '/images/p1.jpg',
+        description: 'High quality product',
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '1',
+        name: 'Nike Slim Shirt',
+        image: '/images/p1.jpg',
+        description: 'High quality product',
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+    },
+    {
+        _id: '1',
+        name: 'Nike Slim Shirt',
+        image: '/images/p1.jpg',
+        description: 'High quality product',
+        price: 120,
+        countInStock: 10,
+        rating: 4.5,
+        numReviews: 10,
+
+    }
+];
